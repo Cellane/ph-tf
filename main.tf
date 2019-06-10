e470fea0d4211d5eb8676047b8e7273d23177b47
Milan Vit <milanvit@milanvit.net>
2019-06-10 11:48:52 +0900

    Added production environment

--- main.tf
+++ main.tf
@@ -25,3 +25,20 @@ module "stg" {
   key_name_backend = "UatPawrentsBackend"
   key_name_db      = "UatPawrentsDatabase"
 }
+
+module "prod" {
+  source = "./environment"
+
+  environment_name          = "prod"
+  vpc_cidr_block            = "10.2.0.0/16"
+  bastion_security_group_id = "${aws_security_group.bastion_sg.id}"
+
+  ami_backend = "ami-0862747aa8ec16761"
+  ami_db      = "ami-068b4585f274ce8ec"
+
+  instance_type_backend = "t2.medium"
+  instance_type_db      = "t2.medium"
+
+  key_name_backend = "ProdPawrentsBackend"
+  key_name_db      = "ProdPawrentsDatabase"
+}
--- output.tf
+++ output.tf
@@ -21,3 +21,15 @@ output "stg_backend_private_ip" {
 output "stg_backend_public_ip" {
   value = "${module.stg.backend_public_ip}"
 }
+
+output "prod_db_private_ip" {
+  value = "${module.prod.db_private_ip}"
+}
+
+output "prod_backend_private_ip" {
+  value = "${module.prod.backend_private_ip}"
+}
+
+output "prod_backend_public_ip" {
+  value = "${module.prod.backend_public_ip}"
+}
